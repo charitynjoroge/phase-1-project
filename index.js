c9dce9b19562e9441a9ec67a8dc3eef3681611d4
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-04-09 15:41:21 +0300

    update

---
+++ index.js
@@ -0,0 +1,88 @@
+document.addEventListener("DOMContentLoaded", () => {
+   const images = document.getElementById("images")
+   let currentIndex = 1
+   let likeCount = 0
+   let dislikeCount = 0
+   
+
+
+
+   function displayImage(){
+    images.src = `https://picsum.photos/id/${currentIndex}/200/300`
+    //reset likes and dislikes for a new image 
+    document.addEventListener("click", (e) => {
+        e.preventDefault();
+    })
+}
+
+document.getElementById("previousButton").addEventListener("click",function(){
+    if(currentIndex < 1)currentIndex = 1
+    currentIndex --
+    displayImage() 
+})
+
+document.getElementById("nextButton").addEventListener("click",function(){
+    currentIndex ++
+    displayImage()
+})
+console.log();
+
+    
+    let isLiked = false
+   let isDisLiked = false
+    document.getElementById("likeCount").innerText = likeCount;
+    document.getElementById("dislikeCount").innerText = dislikeCount;
+
+
+document.getElementById("likeButton").addEventListener("click", function(){
+    if(!isLiked){
+    likeCount ++; 
+    document.getElementById("likeCount").innerText = likeCount
+    isLiked = true;
+
+    if(isDisLiked){
+       dislikeCount --;
+        document.getElementById("dislikeCount").innerText = dislikeCount
+        isDisLiked = false
+    }
+    }
+})
+
+
+
+document.getElementById("dislikeButton").addEventListener("click", function(){
+    if(!isDisLiked){
+    dislikeCount ++;
+    document.getElementById("dislikeCount").innerText = dislikeCount
+    isDisLiked = true
+
+    if(isLiked){
+       likeCount --;
+        document.getElementById("likeCount").innerText = likeCount
+        isLiked = false
+    }
+
+    }
+})
+
+
+displayImage();
+
+
+document.getElementById("commentButton").addEventListener("click",function(){
+    let commentText = document.getElementById("commentText").value;
+    let commentElement = document.createElement("p")
+    commentElement.textContent = commentText;
+
+    document.getElementById("commentDisplay").appendChild(commentElement);
+    document.getElementById("commentText").value = "";
+
+commentText.forEach(images => {
+        console.log(images)
+        
+    });
+})
+
+
+
+})
